aad00ffe8368967fe7e3b094c18564136dd3760f
YameenShariz <yameenshariz2006@gmail.com>
2024-12-06 20:45:01 +0530

    home page for mobile version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,7 @@ export default {
       },
       backgroundImage: {
         'gradient-bg': "linear-gradient(to right, white 28rem, rgba(255, 255, 255, 0) 100%), url('/images/background.png')",
+        'gradient-bg-m': "linear-gradient(to top, black 40%, rgba(255, 255, 255, 0) 100%), url('/images/bgHomePageMobile.png')"
       },
     },
   },
