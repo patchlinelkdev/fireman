2cc7cb6177bf75ce48542579f14c68d7c57d2fb6
YameenShariz <yameenshariz2006@gmail.com>
2024-12-07 13:09:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import GetStartedBtn from './components/HomePage/GetStartedBtn';
 import LoginBtn from './components/HomePage/LoginBtn';
 import ProfileBtn from './components/HomePage/ProfileBtn';
 import SignupBtn from './components/HomePage/SignupBtn';
@@ -25,9 +24,7 @@ export default function Home() {
             FIREMAN
           </div>
         </div>
-      </div>
-
-      
+      </div>  
     </>
   );
 }
