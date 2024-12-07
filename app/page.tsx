f48f63b4f6abb1f9d6f33982f34ec623fd293975
YameenShariz <yameenshariz2006@gmail.com>
2024-12-07 21:06:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,13 +26,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="lg:hidden md:hidden">
-          <h1 className="text-3xl font-bold">One Stop Home Services</h1>
-          <p className="text-wrap text-sm">
-            Your one-stop solution for all home services needs! From plumbing
-            repairs to cleaning services, we've got you covered.
-          </p>
-      </div>
+      <h1 className="text-3xl font-bold">One Stop Home Services</h1>
+      <p className="text-wrap text-sm">
+        Your one-stop solution for all home services needs! From plumbing
+        repairs to cleaning services, we've got you covered.
+      </p>
     </>
   );
 }
