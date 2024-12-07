6ed16cd969c2936495f54ad59a992a12cb238f68
YameenShariz <yameenshariz2006@gmail.com>
2024-12-07 21:13:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GetStartedBtn from './components/HomePage/GetStartedBtn';
 import LoginBtn from './components/HomePage/LoginBtn';
 import ProfileBtn from './components/HomePage/ProfileBtn';
 import SignupBtn from './components/HomePage/SignupBtn';
@@ -29,6 +30,12 @@ export default function Home() {
       <div className="lg:hidden md:hidden">
         <div className="flex justify-center items-end h-screen w-screen bg-gradient-bg-m bg-center bg-cover bg-no-repeat">
           <div className="text-center mx-10 mb-12 text-white">
+            <div className='px-6'>
+              <h1 className="text-3xl font-bold">One Stop Home Services</h1>
+            </div>
+            <div>
+              <GetStartedBtn />
+            </div>
           </div>
         </div>
       </div>
