cd9e39f929c86cd7738d1a1b393ec142a7b0d988
YameenShariz <yameenshariz2006@gmail.com>
2024-12-07 13:13:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GetStartedBtn from './components/HomePage/GetStartedBtn';
 import LoginBtn from './components/HomePage/LoginBtn';
 import ProfileBtn from './components/HomePage/ProfileBtn';
 import SignupBtn from './components/HomePage/SignupBtn';
@@ -24,7 +25,24 @@ export default function Home() {
             FIREMAN
           </div>
         </div>
-      </div>  
+      </div>
+
+      <div className="lg:hidden md:hidden">
+        <div className="flex justify-center items-end h-screen w-screen bg-gradient-bg-m bg-center bg-cover bg-no-repeat">
+          <div className="text-center mx-10 mb-12 text-white">
+            <div className="px-6">
+              <h1 className="text-3xl font-bold">One Stop Home Services</h1>
+            </div>
+
+            <div className="pt-7 pb-12">
+              <p className="text-wrap text-sm">
+                Your one-stop solution for all home services needs! From
+                plumbing repairs to cleaning services, we've got you covered.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
